d27f19a4b11a704987c3e4884962e0b1346f792b
Internaut007 <70135745+internauten@users.noreply.github.com>
2026-05-22 12:41:14 +0200

    Update version to 1.2.1

--- internautenproductai/internautenproductai.php
+++ internautenproductai/internautenproductai.php
@@ -14,7 +14,7 @@ class InternautenProductAi extends Module
     {
         $this->name = 'internautenproductai';
         $this->tab = 'administration';
-        $this->version = '1.2.0';
+        $this->version = '1.2.1';
         $this->author = 'die.internauten.ch GmbH';
         $this->need_instance = 0;
         $this->bootstrap = true;
